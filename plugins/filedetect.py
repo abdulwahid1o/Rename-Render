dcd1656284b0ef01fda568305088b7923e4b5cd1
abdulwahid1o <110869975+abdulwahid1o@users.noreply.github.com>
2024-08-19 23:09:17 +0530

    Update filedetect.py

--- plugins/filedetect.py
+++ plugins/filedetect.py
@@ -1,34 +1,30 @@
 from pyrogram import Client, filters
 from pyrogram.enums import MessageMediaType
-from pyrogram.types import ForceReply
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 @Client.on_message(filters.private & filters.reply)
 async def refunc(client, message):
     reply_message = message.reply_to_message
-    if (reply_message.reply_markup) and isinstance(reply_message.reply_markup, ForceReply):
-       new_name = message.text 
-       await message.delete() 
-       msg = await client.get_messages(message.chat.id, reply_message.id)
-       file = msg.reply_to_message
-       media = getattr(file, file.media.value)
+    if reply_message.reply_markup and isinstance(reply_message.reply_markup, ForceReply):
+        new_name = message.text
+        await message.delete()
+        msg = await client.get_messages(message.chat.id, reply_message.id)
+        file = msg.reply_to_message
+        media = getattr(file, file.media.value)
 
-       if not "." in new_name:
-           if "." in media.file_name:
-               extn = media.file_name.rsplit('.', 1)[-1]
-           else:
-               extn = "mp4"  # Default extension set to .mp4 for videos
-           new_name = new_name + "." + extn
+        if not "." in new_name:
+            extn = media.file_name.rsplit('.', 1)[-1] if "." in media.file_name else "mp4"
+            new_name = new_name + "." + extn
+        await reply_message.delete()
 
-       await reply_message.delete()
+        button = [[InlineKeyboardButton("🎥 𝚅𝙸𝙳𝙴𝙾", callback_data="upload_video")]]
+        if file.media in [MessageMediaType.DOCUMENT]:
+            button.append([InlineKeyboardButton("📁 𝙳𝙾𝙲𝚄𝙼𝙴𝙽𝚃𝚂", callback_data="upload_document")])
+        elif file.media == MessageMediaType.AUDIO:
+            button.append([InlineKeyboardButton("🎵 𝙰𝚄𝙳𝙸𝙾", callback_data="upload_audio")])
 
-       # Automatically send the file as a video
-       await client.send_message(
-           chat_id=message.chat.id,
-           text=f"Processing file as video: **{new_name}**",
-           reply_to_message_id=file.id
-       )
-       await client.send_video(
-           chat_id=message.chat.id,
-           video=file.file_id,
-           caption=f"**{new_name}**"
-       )
+        await message.reply_text(
+            f"**Select the output file type**\n**• File Name :-**```{new_name}```",
+            reply_to_message_id=file.id,
+            reply_markup=InlineKeyboardMarkup(button)
+        )

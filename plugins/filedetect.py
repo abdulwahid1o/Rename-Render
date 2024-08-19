b7e4ca2ba96ef70d171fad70af74085e1abeff7a
abdulwahid1o <110869975+abdulwahid1o@users.noreply.github.com>
2024-08-19 22:52:20 +0530

    Update filedetect.py

--- plugins/filedetect.py
+++ plugins/filedetect.py
@@ -1,7 +1,28 @@
-# Automatically set the file type to video
-if file.media in [MessageMediaType.VIDEO, MessageMediaType.DOCUMENT, MessageMediaType.AUDIO]:
-    await message.reply_text(
-        f"**Processing file as video.**\n**• File Name :-**```{new_name}```",
-        reply_to_message_id=file.id,
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🚀 Uploading as Video", callback_data="upload_video")]])
-    )
+from pyrogram import Client, filters
+from pyrogram.enums import MessageMediaType
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply
+
+@Client.on_message(filters.private & filters.reply)
+async def refunc(client, message):
+    reply_message = message.reply_to_message
+    if (reply_message.reply_markup) and isinstance(reply_message.reply_markup, ForceReply):
+       new_name = message.text 
+       await message.delete() 
+       msg = await client.get_messages(message.chat.id, reply_message.id)
+       file = msg.reply_to_message
+       media = getattr(file, file.media.value)
+       if not "." in new_name:
+          if "." in media.file_name:
+              extn = media.file_name.rsplit('.', 1)[-1]
+          else:
+              extn = "mkv"
+          new_name = new_name + "." + extn
+       await reply_message.delete()
+
+       # Ensure this code is inside the async function
+       if file.media in [MessageMediaType.VIDEO, MessageMediaType.DOCUMENT, MessageMediaType.AUDIO]:
+           await message.reply_text(
+               f"**Processing file as video.**\n**• File Name :-**```{new_name}```",
+               reply_to_message_id=file.id,
+               reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🚀 Uploading as Video", callback_data="upload_video")]])
+           )

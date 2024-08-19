3e6ea13f5b76b8aa50b5eab183c88b660c11cf91
abdulwahid1o <110869975+abdulwahid1o@users.noreply.github.com>
2024-08-19 15:26:49 +0530

    Update txt.py

--- helper/txt.py
+++ helper/txt.py
@@ -14,7 +14,7 @@ class mr(object):
     ABOUT_TXT = """
 ╭───────────⍟
 ├🤖 My Name is : {}
-├👑 CHANNEL : <a href=https://t.me/VJ_Bots>VJ Bots</a> 
+├👑 CHANNEL : <a href=https://t.me/abdul_wahid101>VJ Bots</a> 
 ├📕 LIBRARY : <a href=https://github.com/pyrogram>Pyrogram</a>
 ├✏️ LANGUAGE : <a href=https://www.python.org>Python 3</a>
 ├💾 DATABASE : <a href=https://cloud.mongodb.com>MongoDB</a>

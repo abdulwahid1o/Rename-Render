27b7b3fe50ad1ba39c2d5abb7e5cdb5bcbda48ae
abdulwahid1o <110869975+abdulwahid1o@users.noreply.github.com>
2024-08-19 23:56:54 +0530

    Update filedetect.py

--- plugins/filedetect.py
+++ plugins/filedetect.py
@@ -1,24 +1,27 @@
+from pyrogram import Client, filters
+from pyrogram.types import ForceReply
+
 @Client.on_message(filters.private & filters.reply)
 async def refunc(client, message):
     reply_message = message.reply_to_message
     if (reply_message.reply_markup) and isinstance(reply_message.reply_markup, ForceReply):
-       new_name = message.text 
-       await message.delete() 
-       msg = await client.get_messages(message.chat.id, reply_message.id)
-       file = msg.reply_to_message
-       media = getattr(file, file.media.value)
-       if not "." in new_name:
-          if "." in media.file_name:
-              extn = media.file_name.rsplit('.', 1)[-1]
-          else:
-              extn = "mkv"
-          new_name = new_name + "." + extn
-       await reply_message.delete()
+        new_name = message.text 
+        await message.delete() 
+        msg = await client.get_messages(message.chat.id, reply_message.id)
+        file = msg.reply_to_message
+        media = getattr(file, file.media.value)
+        if not "." in new_name:
+            if "." in media.file_name:
+                extn = media.file_name.rsplit('.', 1)[-1]
+            else:
+                extn = "mkv"
+            new_name = new_name + "." + extn
+        await reply_message.delete()
 
-       await client.send_message(
-           message.chat.id,
-           f"File `{new_name}` is being processed as a video...",
-           reply_to_message_id=file.id,
-       )
-       # Trigger the upload process directly
-       await upload(client, msg)
+        await client.send_message(
+            message.chat.id,
+            f"File `{new_name}` is being processed as a video...",
+            reply_to_message_id=file.id,
+        )
+        # Trigger the upload process directly
+        await upload(client, msg)

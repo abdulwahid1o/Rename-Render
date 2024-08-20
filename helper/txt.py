780a17155a50f2f239b76f07d6e56826325eedfd
abdulwahid1o <110869975+abdulwahid1o@users.noreply.github.com>
2024-08-20 14:56:11 +0530

    Update txt.py

--- helper/txt.py
+++ helper/txt.py
@@ -2,7 +2,7 @@
 
 class mr(object):
     PROGRESS_BAR = """\n
-╭━━━━❰ VJ Bot Renaming... ❱━➣
+╭━━━━❰ Video Coverter Bot Renaming... ❱━➣
 ┣⪼ 🗂️ : {1} | {2}
 ┣⪼ ⏳️ : {0}%
 ┣⪼ 🚀 : {3}/s
@@ -12,7 +12,7 @@ class mr(object):
     ABOUT_TXT = """
 ╭───────────⍟
 ├🤖 My Name is : {}
-├👑 CHANNEL : <a href=https://t.me/VJ_Bots>VJ Bots</a> 
+├👑 CHANNEL : <a href=https://t.me/abdul_wahid101>Video Converter Bot</a> 
 ├📕 LIBRARY : <a href=https://github.com/pyrogram>Pyrogram</a>
 ├✏️ LANGUAGE : <a href=https://www.python.org>Python 3</a>
 ├💾 DATABASE : <a href=https://cloud.mongodb.com>MongoDB</a>

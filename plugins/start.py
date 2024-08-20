d3f2b9a437aec029a34421ddd87dfdf1e5f79fb8
abdulwahid1o <110869975+abdulwahid1o@users.noreply.github.com>
2024-08-20 15:18:44 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -14,13 +14,13 @@ async def start(client, message):
     user = message.from_user
     if not await db.is_user_exist(user.id):
         await db.add_user(user.id)             
-    txt=f"👋 Hello Developer {user.mention} \n\nI am an Advance file Renamer and file Converter BOT with Custom thumbnail support.\n\nSend me any video or document!"
-    button=InlineKeyboardMarkup([[
+    txt = f"👋 Hello Developer {user.mention} \n\nI am an Advance file Renamer and file Converter BOT with Custom thumbnail support.\n\nSend me any video or document!"
+    button = InlineKeyboardMarkup([[
         InlineKeyboardButton(' Support', url='https://t.me/abdul_wahid101')
         ],[
-        InlineKeyboardButton(' About', callback_data='about') ],
-        [InlineKeyboardButton(' Help', callback_data='help') ]
-        ])
+        InlineKeyboardButton(' About', callback_data='about')],
+        [InlineKeyboardButton(' Help', callback_data='help')]
+    ])
     if START_PIC:
         await message.reply_photo(START_PIC, caption=txt, reply_markup=button)       
     else:
@@ -42,15 +42,14 @@ async def rename_start(client, message):
     fileid = file.file_id
     try:
         text = f"""**__What do you want me to do with this file.?__**\n\n**File Name** :- `{filename}`\n\n**File Size** :- `{filesize}`"""
-        buttons = [[ InlineKeyboardButton("📝 𝚂𝚃𝙰𝚁𝚃 𝚁𝙴𝙽𝙰𝙼𝙴 📝", callback_data="rename") ],
-                   [ InlineKeyboardButton("✖️ 𝙲𝙰𝙽𝙲𝙴𝙻 ✖️", callback_data="cancel") ]]
+        buttons = [[InlineKeyboardButton("📝 𝚂𝚃𝙰𝚁𝚃 𝚁𝙴𝙽𝙰𝙼𝙴 📝", callback_data="rename")],
+                   [InlineKeyboardButton("✖️ 𝙲𝙰𝙽𝙲𝙴𝙻 ✖️", callback_data="cancel")]]
         await message.reply_text(text=text, reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(buttons))
         await sleep(FLOOD)
     except FloodWait as e:
         await sleep(e.value)
-        text = f"""**__What do you want me to do with this file.?__**\n\n**File Name** :- `{filename}`\n\n**File Size** :- `{filesize}`"""
-        buttons = [[ InlineKeyboardButton("📝 𝚂𝚃𝙰𝚁𝚃 𝚁𝙴𝙽𝙰𝙼𝙴 📝", callback_data="rename") ],
-                   [ InlineKeyboardButton("✖️ 𝙲𝙰𝙽𝙲𝙴𝙻 ✖️", callback_data="cancel") ]]
+        buttons = [[InlineKeyboardButton("📝 𝚂𝚃𝙰𝚁𝚃 𝚁𝙴𝙽𝙰𝙼𝙴 📝", callback_data="rename")],
+                   [InlineKeyboardButton("✖️ 𝙲𝙰𝙽𝙲𝙴𝙻 ✖️", callback_data="cancel")]]
         await message.reply_text(text=text, reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(buttons))
     except:
         pass
@@ -60,43 +59,38 @@ async def cb_handler(client, query: CallbackQuery):
     data = query.data 
     if data == "start":
         await query.message.edit_text(
-            text=f"""👋 Hello Developer {query.from_user.mention} \n\nI am an Advance file Renamer and file Converter BOT with permanent and custom thumbnail support.\n\nSend me any video or document!""",
-            reply_markup=InlineKeyboardMarkup( [[
-        InlineKeyboardButton(' Support', url='https://t.me/abdul_wahid101')
-        ],[
-        InlineKeyboardButton(' About', callback_data='about'),
-        InlineKeyboardButton(' Help', callback_data='help')
-        ]]
+            text=f"👋 Hello Developer {query.from_user.mention} \n\nI am an Advance file Renamer and file Converter BOT with permanent and custom thumbnail support.\n\nSend me any video or document!",
+            reply_markup=InlineKeyboardMarkup([[
+                InlineKeyboardButton(' Support', url='https://t.me/abdul_wahid101')
+                ],[
+                InlineKeyboardButton(' About', callback_data='about'),
+                InlineKeyboardButton(' Help', callback_data='help')
+            ]])
         )
     elif data == "help":
         await query.message.edit_text(
             text=mr.HELP_TXT,
-            reply_markup=InlineKeyboardMarkup( [[
-               InlineKeyboardButton(" 𝙲𝙻𝙾𝚂𝙴", callback_data = "close"),
-               InlineKeyboardButton(" 𝙱𝙰𝙲𝙺", callback_data = "start")
-               ]]
-            )
+            reply_markup=InlineKeyboardMarkup([[
+               InlineKeyboardButton(" 𝙲𝙻𝙾𝚂𝙴", callback_data="close"),
+               InlineKeyboardButton(" 𝙱𝙰𝙲𝙺", callback_data="start")
+            ]])
         )
     elif data == "about":
         await query.message.edit_text(
             text=mr.ABOUT_TXT.format(client.mention),
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup( [
-               [
-               InlineKeyboardButton(" 𝙲𝙻𝙾𝚂𝙴", callback_data = "close"),
-               InlineKeyboardButton(" 𝙱𝙰𝙲𝙺", callback_data = "start")
-               ]]
-            )
+            reply_markup=InlineKeyboardMarkup([[
+               InlineKeyboardButton(" 𝙲𝙻𝙾𝚂𝙴", callback_data="close"),
+               InlineKeyboardButton(" 𝙱𝙰𝙲𝙺", callback_data="start")
+            ]])
         )
     elif data == "dev":
         await query.message.edit_text(
             text=mr.DEV_TXT,
-            reply_markup=InlineKeyboardMarkup( [
-               [
-               InlineKeyboardButton(" 𝙲𝙻𝙾𝚂𝙴", callback_data = "close"),
-               InlineKeyboardButton(" 𝙱𝙰𝙲𝙺", callback_data = "start")
-               ]]
-            )
+            reply_markup=InlineKeyboardMarkup([[
+               InlineKeyboardButton(" 𝙲𝙻𝙾𝚂𝙴", callback_data="close"),
+               InlineKeyboardButton(" 𝙱𝙰𝙲𝙺", callback_data="start")
+            ]])
         )
     elif data == "close":
         try:

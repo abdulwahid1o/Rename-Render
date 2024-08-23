18bee119a49906226ca742682ea1767018f816a7
abdulwahid1o <110869975+abdulwahid1o@users.noreply.github.com>
2024-08-23 17:52:26 +0530

    Update thumbfuc.py

--- plugins/thumbfuc.py
+++ plugins/thumbfuc.py
@@ -1,34 +1,28 @@
+# Don't Remove Credit @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 from pyrogram import Client, filters
 from helper.database import db
-import subprocess
 
 @Client.on_message(filters.private & filters.command(['viewthumb']))
 async def viewthumb(client, message):    
     thumb = await db.get_thumbnail(message.from_user.id)
     if thumb:
        await client.send_photo(
-           chat_id=message.chat.id, 
-           photo=thumb)
+	   chat_id=message.chat.id, 
+	   photo=thumb)
     else:
         await message.reply_text("😔**Sorry ! No thumbnail found...**😔") 
-
+		
 @Client.on_message(filters.private & filters.command(['delthumb']))
 async def removethumb(client, message):
     await db.set_thumbnail(message.from_user.id, file_id=None)
     await message.reply_text("**Thumbnail deleted successfully**✅️")
-
-@Client.on_message(filters.private & filters.video)
+	
+@Client.on_message(filters.private & filters.photo)
 async def addthumbs(client, message):
-    video_path = await message.download() # Download the video
-    thumb_path = extract_thumbnail(video_path) # Extract the thumbnail
     LazyDev = await message.reply_text("Please Wait ...")
-    await db.set_thumbnail(message.from_user.id, file_id=thumb_path)                
+    await db.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
     await LazyDev.edit("**Thumbnail saved successfully**✅️")
-
-def extract_thumbnail(video_path):
-    thumb_path = video_path.replace('.mp4', '_thumb.jpg')
-    command = [
-        'ffmpeg', '-i', video_path, '-ss', '00:00:15.000', '-vframes', '1', thumb_path
-    ]
-    subprocess.run(command, check=True)
-    return thumb_path
+	

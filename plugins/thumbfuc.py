d7d3d8909f55ea8fc9277463dd4e79e7b996a5a7
abdulwahid1o <110869975+abdulwahid1o@users.noreply.github.com>
2024-08-23 17:07:53 +0530

    Update thumbfuc.py

--- plugins/thumbfuc.py
+++ plugins/thumbfuc.py
@@ -1,5 +1,6 @@
 from pyrogram import Client, filters
 from helper.database import db
+import subprocess
 
 @Client.on_message(filters.private & filters.command(['viewthumb']))
 async def viewthumb(client, message):    
@@ -16,8 +17,18 @@ async def removethumb(client, message):
     await db.set_thumbnail(message.from_user.id, file_id=None)
     await message.reply_text("**Thumbnail deleted successfully**✅️")
 
-@Client.on_message(filters.private & filters.photo)
+@Client.on_message(filters.private & filters.video)
 async def addthumbs(client, message):
+    video_path = await message.download() # Download the video
+    thumb_path = extract_thumbnail(video_path) # Extract the thumbnail
     LazyDev = await message.reply_text("Please Wait ...")
-    await db.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
+    await db.set_thumbnail(message.from_user.id, file_id=thumb_path)                
     await LazyDev.edit("**Thumbnail saved successfully**✅️")
+
+def extract_thumbnail(video_path):
+    thumb_path = video_path.replace('.mp4', '_thumb.jpg')
+    command = [
+        'ffmpeg', '-i', video_path, '-ss', '00:00:15.000', '-vframes', '1', thumb_path
+    ]
+    subprocess.run(command, check=True)
+    return thumb_path

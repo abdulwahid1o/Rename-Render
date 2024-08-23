5b976bcf12a9059b522f6d7244721f1ff2bf2826
abdulwahid1o <110869975+abdulwahid1o@users.noreply.github.com>
2024-08-23 15:56:16 +0530

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -22,11 +22,9 @@ async def rename(bot, update):
     user_id = update.message.chat.id
     date = update.message.date
     await update.message.delete()
-    await update.message.reply_text(
-        "__𝙿𝚕𝚎𝚊𝚜𝚎 𝙴𝚗𝚝𝚎𝚛 𝙽𝚎𝚠 𝙵𝚒𝚕𝚎𝙽𝚊𝚖𝚎...__\n\n**File Name** :- `{filename}`",
+    await update.message.reply_text("__𝙿𝚕𝚎𝚊𝚜𝚎 𝙴𝚗𝚝𝚎𝚛 𝙽𝚎𝚠 𝙵𝚒𝚕𝚎𝙽𝚊𝚖𝚎...__\n\n**File Name** :- `{filename}`",
         reply_to_message_id=update.message.reply_to_message.id,
-        reply_markup=ForceReply(True)
-    )
+        reply_markup=ForceReply(True))
 
 @Client.on_callback_query(filters.regex("upload"))
 async def doc(bot,update):
